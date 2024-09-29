a1ff0d49fd3a4988fa9710939ae0818f5cb77cac
vipcxj <vipcxj@126.com>
2024-09-30 01:38:54 +0800

    wip

--- src/public/cfgo/measure.hpp
+++ src/public/cfgo/measure.hpp
@@ -3,35 +3,135 @@
 
 #include <chrono>
 #include <vector>
+#include <list>
+#include <cassert>
+#include <concepts>
+#include <algorithm>
 
 namespace cfgo
 {
+
+    template<typename T>
+    concept Measureable = requires() {
+        { T{0} };
+    } && requires(T a, T b) {
+        { a < b } -> std::same_as<bool>;
+    };
+    template<typename T>
+    concept Meanable = requires(T a, std::size_t n) {
+        { a / n } -> std::convertible_to<T>;
+    };
+
     template<typename T>
     class BaseMeasure
     {
-    private:
-        std::vector<T> m_maxes;
-        std::vector<T> m_mines;
-        std::vector<T> m_latests;
-        T m_sum;
-        T m_num;
-        T m_capacity;
     public:
-        TimeMeasure();
-        ~TimeMeasure();
-        void update(const T & value);
+        using Vec = std::vector<T>;
+        using List = std::list<T>;
+        using Size = Vec::size_type;
+    private:
+        Vec m_maxes;
+        Vec m_mines;
+        List m_latests;
+        T m_sum {0};
+        std::size_t m_num {0};
+        Size m_capacity;
+    public:
+        BaseMeasure(Size capacity): m_capacity(capacity)
+        {
+            assert(m_capacity > 0);
+        };
+        ~BaseMeasure() = default;
+        void update(const T & value)
+        {
+            m_sum = m_sum + value;
+            ++ m_num;
+            m_maxes.push_back(value);
+            std::sort(m_maxes.rbegin(), m_maxes.rend());
+            if (m_maxes.size() == m_capacity)
+            {
+                m_maxes.pop_back();
+            }
+            m_mines.push_back(value);
+            std::sort(m_mines.begin(), m_mines.end());
+            if (m_mines.size() == m_capacity)
+            {
+                m_mines.pop_back();
+            }
+            m_latests.push_back(value);
+            if (m_latests.size() == m_capacity)
+            {
+                m_latests.push_front();
+            }
+        }
 
-        const std::vector<T> & max_vec() const noexcept;
-        const std::vector<T> & min_vec() const noexcept;
-        const std::vector<T> & latest_vec() const noexcept;
-        T sum() const noexcept;
-        std::uint64_t num() const noexcept;
-        T max() const noexcept;
-        T min() const noexcept;
-        T mean() const noexcept; 
+        const Vec & max_vec() const noexcept
+        {
+            return m_maxes;
+        }
+        const Vec & min_vec() const noexcept
+        {
+            return m_mines;
+        }
+        const List & latest_list() const noexcept
+        {
+            return m_latests;
+        }
+        T sum() const noexcept
+        {
+            return m_sum;
+        }
+        std::size_t num() const noexcept
+        {
+            return m_num;
+        }
+        std::optional<T> max() const noexcept
+        {
+            if (m_maxes.empty())
+            {
+                return std::nullopt;
+            }
+            else
+            {
+                return m_maxes[0];
+            }
+        }
+        std::optional<T> min() const noexcept
+        {
+            if (m_mines.empty())
+            {
+                return std::nullopt;
+            }
+            else
+            {
+                return m_mines[0];
+            }
+        }
+        std::optional<T> mean() const noexcept requires Meanable<T>
+        {
+            if (m_maxes.empty())
+            {
+                return std::nullopt;
+            }
+            else
+            {
+                return m_sum / m_num;
+            }
+        }
+    };
+
+    using HighDuration = std::chrono::high_resolution_clock::duration;
+    using HighTimePoint = std::chrono::high_resolution_clock::time_point;
+    
+    class DurationMeasure : public BaseMeasure<HighDuration>
+    {
+    private:
+        /* data */
+    public:
+        DurationMeasure(DurationMeasure::Size capacity): BaseMeasure<HighDuration>(capacity) {}
+        ~DurationMeasure() = default;
     };
     
-
 } // namespace cfgo
 
 
